cdc2aaa08de926ee8e76fee9eda81f246a6d3db8
marco6589 <136659659+marco6589@users.noreply.github.com>
2023-06-16 22:03:02 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = my_fruit_list.set_index ('Fruit')
 fruits_selected = streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
-streamlit.dataframe (fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -29,7 +29,7 @@ try:
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe (fruityvice_normalised)
+    streamlit.dataframe(fruityvice_normalised)
 except URLError as e:
   streamlit.error("opps")
 
