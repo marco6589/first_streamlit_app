60a3966002960b6c30f055986eabddb7e080ddb1
marco6589 <136659659+marco6589@users.noreply.github.com>
2023-06-16 22:25:04 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-import pandas
 import requests
 import snowflake.connector
+import pandas
 from urllib.error import URLError
 
 streamlit.title ('My Parents New Healthy Dinner') 
@@ -18,8 +18,9 @@ my_fruit_list = my_fruit_list.set_index ('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 # display the table on the page
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe (fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
